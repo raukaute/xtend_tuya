d5f889c9025c35368f4c2e87ac799a2959057dce
azerty9971 <3135352+azerty9971@users.noreply.github.com>
2026-03-22 12:45:26 +0100

    Update entity.py

--- custom_components/xtend_tuya/entity.py
+++ custom_components/xtend_tuya/entity.py
@@ -800,5 +800,5 @@ class XTEntity(TuyaEntity):
         if dpcode_information.dpcode in DPCODE_PREFERED_DEVICE_CLASS:
             if DPCODE_PREFERED_DEVICE_CLASS[dpcode_information.dpcode] is None or DPCODE_PREFERED_DEVICE_CLASS[dpcode_information.dpcode] in proposed_device_class:
                 return DPCODE_PREFERED_DEVICE_CLASS[dpcode_information.dpcode]
-        LOGGER.warning(f"Multiple possible device class {proposed_device_class} for unit {dpcode_information.unit} on device {device.name} ({dpcode_information.dpcode}), unable to determine the most probable one, returning None. Plese report to developer.")
+        LOGGER.warning(f"Multiple possible device class {proposed_device_class} for unit {dpcode_information.unit} on device {device.name} ({dpcode_information.dpcode}), unable to determine the most probable one, returning None. Plese report to developer.", stack_info=True)
         return None
