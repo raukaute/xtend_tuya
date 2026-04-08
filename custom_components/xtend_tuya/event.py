b64f0a7b9caae018b5fdfa394ee6a6d27375901a
azerty9971 <3135352+azerty9971@users.noreply.github.com>
2026-04-08 16:41:35 +0200

    Update event.py

--- custom_components/xtend_tuya/event.py
+++ custom_components/xtend_tuya/event.py
@@ -208,14 +208,14 @@ EVENTS: dict[str, tuple[XTEventEntityDescription, ...]] = {
         XTEventEntityDescription(
             key=XTDPCode.UNLOCK_HAND,
             translation_key="unlock_user",
-            translation_placeholders={"user_type": "Face"},
+            translation_placeholders={"user_type": "Hand"},
             device_class=None,
             wrapper_class=IntegerEventWrapper,
         ),
         XTEventEntityDescription(
             key=XTDPCode.UNLOCK_DYNAMIC,
             translation_key="unlock_user",
-            translation_placeholders={"user_type": "Face"},
+            translation_placeholders={"user_type": "Dynamic"},
             device_class=None,
             wrapper_class=IntegerEventWrapper,
         ),
