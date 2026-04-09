db8f95cfd5e01f43192f206673b19cf45b83f41f
azerty9971 <3135352+azerty9971@users.noreply.github.com>
2026-04-09 19:30:39 +0200

    More device debug

--- custom_components/xtend_tuya/multi_manager/managers/tuya_iot/xt_tuya_iot_mq.py
+++ custom_components/xtend_tuya/multi_manager/managers/tuya_iot/xt_tuya_iot_mq.py
@@ -1,5 +1,14 @@
 from __future__ import annotations
 
+from typing import Any
+from paho.mqtt import client as mqtt
+from paho.mqtt.reasoncodes import (
+    ReasonCode as mqtt_ReasonCode,
+)
+from paho.mqtt.properties import (
+    Properties as mqtt_Properties,
+)
+
 from ....lib.tuya_iot import (
     TuyaOpenMQ,
 )
@@ -11,6 +20,11 @@ from .xt_tuya_iot_openapi import (
 )
 import custom_components.xtend_tuya.multi_manager.managers.tuya_iot.xt_tuya_iot_manager as man
 import custom_components.xtend_tuya.multi_manager.managers.tuya_iot.ipc.xt_tuya_iot_ipc_manager as ipc_man
+from ....const import (
+    LOGGER,
+    XTDeviceWatcherCategory,
+    XTDeviceWatcherSpecialDevice,
+)
 
 
 class XTIOTTuyaMQConfig(TuyaMQConfig):
@@ -33,3 +47,24 @@ class XTIOTOpenMQ(TuyaOpenMQ):
             link_id=link_id,
         )
         self.manager = manager
+
+    def _on_connect(
+        self,
+        mqttc: mqtt.Client,
+        user_data: Any,
+        flags,
+        rc: mqtt_ReasonCode,
+        properties: mqtt_Properties | None = None,
+    ):
+        if rc == 0:
+            for key, value in self.mq_config.source_topic.items():
+                mqttc.subscribe(value)
+            if self.manager is not None:
+                self.manager.multi_manager.device_watcher.report_message(
+                    XTDeviceWatcherSpecialDevice.NOT_LINKED_TO_A_DEVICE,
+                    f"Subscribed to topics: {self.mq_config.source_topic=}",
+                    XTDeviceWatcherCategory.MQTT,
+                )
+            # logger.debug(f"[{self.class_id} MQTT] connected and subscribed to topics: {self.mq_config.source_topic}")
+        else:
+            LOGGER.error(f"[{self.class_id} MQTT] connect failed with rc={rc}")
--- custom_components/xtend_tuya/multi_manager/shared/shared_classes.py
+++ custom_components/xtend_tuya/multi_manager/shared/shared_classes.py
@@ -33,7 +33,7 @@ class DeviceWatcher:
             "eb7390e135cc5cd63213qg": XTDeviceWatcherCategory.MQTT | XTDeviceWatcherCategory.PLATFORM_SENSOR,
             # "vdevo172985271302839": XTDeviceWatcherCategory.PLATFORM_EVENT | XTDeviceWatcherCategory.VIRTUAL_STATE,
             # "bf022344b6e0cfd5dafh8e": XTDeviceWatcherCategory.MQTT,
-            # XTDeviceWatcherSpecialDevice.NOT_LINKED_TO_A_DEVICE: XTDeviceWatcherCategory.XT_PERFORMANCE,
+            XTDeviceWatcherSpecialDevice.NOT_LINKED_TO_A_DEVICE: XTDeviceWatcherCategory.XT_PERFORMANCE | XTDeviceWatcherCategory.MQTT,
         }
         self.multi_manager = multi_manager
 
