1732b20869232b9a983aa6eeaa760122d9164797
azerty9971 <3135352+azerty9971@users.noreply.github.com>
2026-04-09 10:24:21 +0200

    Update shared_classes.py

--- custom_components/xtend_tuya/multi_manager/shared/shared_classes.py
+++ custom_components/xtend_tuya/multi_manager/shared/shared_classes.py
@@ -30,7 +30,7 @@ from ...const import (
 class DeviceWatcher:
     def __init__(self, multi_manager: mm.MultiManager) -> None:
         self.watched_dev_id: dict[str, XTDeviceWatcherCategory] = {
-            "bf615b97b0d088e11ee38v": XTDeviceWatcherCategory.MQTT,
+            "eb7390e135cc5cd63213qg": XTDeviceWatcherCategory.MQTT | XTDeviceWatcherCategory.STATUS_CHANGES,
             # "vdevo172985271302839": XTDeviceWatcherCategory.PLATFORM_EVENT | XTDeviceWatcherCategory.VIRTUAL_STATE,
             # "bf022344b6e0cfd5dafh8e": XTDeviceWatcherCategory.MQTT,
             # XTDeviceWatcherSpecialDevice.NOT_LINKED_TO_A_DEVICE: XTDeviceWatcherCategory.XT_PERFORMANCE,
