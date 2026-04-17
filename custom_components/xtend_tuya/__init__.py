0a392de28810c0434ee17c7f37cb9791feb70c20
azerty9971 <3135352+azerty9971@users.noreply.github.com>
2026-04-17 19:36:31 +0200

    Update __init__.py

--- custom_components/xtend_tuya/__init__.py
+++ custom_components/xtend_tuya/__init__.py
@@ -53,6 +53,8 @@ def _getaddrinfo_ipv4_only(host, port, family=0, type=0, proto=0, flags=0):
     tuya_hosts = [
         "apigw.iotbing.com",
         "apigw-sg.iotbing.com",
+        "apigw.tuyaeu.com",
+        "apigw.tuyaus.com",
         "openapi.tuyaus.com",
         "openapi.tuyacn.com",
         "openapi.tuyaeu.com",
@@ -62,6 +64,7 @@ def _getaddrinfo_ipv4_only(host, port, family=0, type=0, proto=0, flags=0):
         "openapi-weaz.tuyaeu.com",
         "m1-sg.iotbing.com",
         "m1.tuyaeu.com",
+        "m1.tuyaus.com",
     ]
     if host and host in tuya_hosts:
         return _original_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
