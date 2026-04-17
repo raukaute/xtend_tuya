8e18d797a48cb19a02ae7552df50ca58ebefe0d9
azerty9971 <3135352+azerty9971@users.noreply.github.com>
2026-04-17 19:40:57 +0200

    Update __init__.py

--- custom_components/xtend_tuya/__init__.py
+++ custom_components/xtend_tuya/__init__.py
@@ -65,6 +65,7 @@ def _getaddrinfo_ipv4_only(host, port, family=0, type=0, proto=0, flags=0):
         "m1-sg.iotbing.com",
         "m1.tuyaeu.com",
         "m1.tuyaus.com",
+        "m1.tuyacn.com",
     ]
     if host and host in tuya_hosts:
         return _original_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
