84debec1566a956348489b79174745412712c576
azerty9971 <3135352+azerty9971@users.noreply.github.com>
2026-04-19 18:44:24 +0200

    Update cloud_fix.py

--- custom_components/xtend_tuya/multi_manager/shared/cloud_fix.py
+++ custom_components/xtend_tuya/multi_manager/shared/cloud_fix.py
@@ -83,7 +83,8 @@ class CloudFixes:
                             LOGGER.warning(f"deserialised {json.loads(converted_value)} for {device.name} => {dpcode_info.dpcode}")
                             new_dptype = TuyaDPType.JSON
                         except Exception as e:
-                            LOGGER.exception(e)
+                            LOGGER.error(f"Exception in JSON conv: {converted_value=} => {e}")
+                            #LOGGER.exception(e)
                 if new_dptype is not None:
                     dpcode_info.dptype = new_dptype
                     device.set_dpcode_information(dpcode_info)
